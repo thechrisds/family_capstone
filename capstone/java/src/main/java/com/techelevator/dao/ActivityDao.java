8db76252dc11acae6755c0e0823344b813db7d08
James Cagle <jamesc.cagle@gmail.com>
2022-09-26 19:15:24 -0400

    activity

--- capstone/java/src/main/java/com/techelevator/dao/ActivityDao.java
+++ capstone/java/src/main/java/com/techelevator/dao/ActivityDao.java
@@ -1,4 +1,24 @@
 package com.techelevator.dao;
 
-public class ActivityDao {
+import com.techelevator.model.Activity;
+
+import java.util.List;
+
+public interface ActivityDao {
+
+    Activity getActivityByActivityId (int activityId);
+
+    List<Activity> getActivityByUserId (int userId);
+
+    List<Activity> getActivityByFamilyId (int familyId);
+
+    Activity createActivity(Activity activity);
+
+    int getTotalReadingTimeByFamilyId (int familyId);
+
+    int getTotalReadingTimeByUserId (int userId);
+
+    //get by date range?
+    //reading a book by minutes read?
+    //
 }
