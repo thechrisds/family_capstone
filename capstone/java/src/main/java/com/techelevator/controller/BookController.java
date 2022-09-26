2ff89f08ac3c838e7fc348e231815a9b30f3a3ff
Chris Shriver <chrisdshriver@gmail.com>
2022-09-26 17:48:51 -0400

    Able to add book

--- capstone/java/src/main/java/com/techelevator/controller/BookController.java
+++ capstone/java/src/main/java/com/techelevator/controller/BookController.java
@@ -4,6 +4,8 @@ package com.techelevator.controller;
 import com.techelevator.dao.BookDao;
 import com.techelevator.model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +26,13 @@ public class BookController {
     public List<Book> getBooks(){
         List<Book> bookList = new ArrayList<>();
         return bookList = bookDao.findAll();
+    }
+
+    @RequestMapping(value = "/books", method = RequestMethod.POST)
+    public void addBook(@RequestBody Book book) throws Exception {
+        if (!bookDao.addBook(book.getBookTitle(), book.getBookAuthor(), book.getIsbn(), book.getGenre(), book.getDescription())){
+            throw new Exception("Adding book failed.");
+        }
     }
 
 
