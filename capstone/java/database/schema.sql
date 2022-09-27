fcd8e2095651cd7bee05d10deae83f8739a83ae2
Daniel Song <songd624@gmail.com>
2022-09-26 17:28:01 -0700

    DB draft, need family account

--- capstone/java/database/schema.sql
+++ capstone/java/database/schema.sql
@@ -1,8 +1,8 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users, library, reading_activity, prizes CASCADE;
+DROP TABLE IF EXISTS users, family_account, library, reading_activity, prizes CASCADE;
 
-DROP SEQUENCE IF EXISTS seq_user_id, seq_library_id, activity_id, prizes_id;
+DROP SEQUENCE IF EXISTS seq_user_id, seq_family_id, seq_library_id, activity_id, prizes_id;
 
 CREATE SEQUENCE seq_user_id
     INCREMENT BY 1
@@ -14,9 +14,18 @@ CREATE TABLE users (
 	username varchar(50) NOT NULL UNIQUE,
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) DEFAULT 'ROLE_USER',
-	family_id int NOT NULL,
-	family_name varchar (50),
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE SEQUENCE seq_family_id
+INCREMENT BY 1
+START WITH 1001
+NO MAXVALUE;
+
+CREATE TABLE family_account (
+    family_id int NOT NULL DEFAULT nextval('seq_family_id'),
+    family_name varchar(50) NOT NULL,
+    CONSTRAINT PK_family_account PRIMARY KEY (family_id)
 );
 
 
@@ -26,6 +35,7 @@ CREATE TABLE users (
 --NO MAXVALUE;
 
 CREATE TABLE library (
+    book_id int NOT NULL UNIQUE,
 	isbn int NOT NULL UNIQUE,
     book_title varchar (100) NOT NULL UNIQUE,
     book_author varchar (50) NOT NULL,
@@ -38,20 +48,19 @@ CREATE TABLE library (
 CREATE TABLE reading_activity (
     activity_id int NOT NULL,
     user_id int NOT NULL,
-    family_id int NOT NULL,
     isbn int NOT NULL,
     minutes_read int DEFAULT 0,
     date_read timestamp,
     notes varchar (250),
     completed boolean DEFAULT false,
     CONSTRAINT PK_reading_activity PRIMARY KEY (activity_id),
-    CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users (user_id),
-    CONSTRAINT FK_family_id FOREIGN KEY (family_id) REFERENCES users (family_id),
+    CONSTRAINT FK_family_account_users FOREIGN KEY (user_id) REFERENCES users (user_id),
 	CONSTRAINT FK_library FOREIGN KEY (isbn) REFERENCES library (isbn)
 );
 
 CREATE TABLE prizes (
     family_id int NOT NULL,
+    user_id int NOT NULL,
     prizes_id int NOT NULL UNIQUE,
     name varchar (50) NOT NULL,
     description varchar (200) NOT NULL,
@@ -60,7 +69,8 @@ CREATE TABLE prizes (
     start_date varchar (20) NOT NULL,
     end_date varchar (20),
     CONSTRAINT PK_prizes PRIMARY KEY (prizes_id),
-    CONSTRAINT FK_family_id FOREIGN KEY (family_id) REFERENCES family_account (family_id)
+    CONSTRAINT FK_family_id FOREIGN KEY (family_id) REFERENCES family_account (family_id),
+    CONSTRAINT FK_users_user_id FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
 
 
