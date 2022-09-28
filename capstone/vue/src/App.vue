b165bfa8f689a460d78edbec2cad63a9f104e760
Chris Shriver <chrisdshriver@gmail.com>
2022-09-28 16:07:48 -0400

    Added Router-Links to top of page

--- capstone/vue/src/App.vue
+++ capstone/vue/src/App.vue
@@ -2,7 +2,10 @@
   <div id="app">
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link v-bind:to="{ name: 'account' }" v-if="$store.state.token != ''">View Account</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'books' }">Books</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'addActivity' }">Add Activity</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>&nbsp;|&nbsp;
     </div>
     <router-view />
   </div>
