b44969db7c91637417d8bf9f60688c31a30f4e1b
James Cagle <jamesc.cagle@gmail.com>
2022-09-30 11:24:13 -0400

    database added format

--- capstone/java/database/data.sql
+++ capstone/java/database/data.sql
@@ -13,9 +13,14 @@ INSERT INTO library (book_id,isbn,book_title,book_author,cover_img,genre, family
 INSERT INTO library (book_id,isbn,book_title,book_author,cover_img,genre, family_id) VALUES (9002, 9781603095020, 'Animal Stories', 'Peter Hoey', 'www.picture.com','bad', 5002);
 INSERT INTO library (book_id,isbn,book_title,book_author,cover_img,genre, family_id) VALUES (9003, 9003, 'Book 3', 'Bob Smith', 'www.picture.com','bad', 5002);
 
+INSERT INTO format (format_id, format_type) VALUES (0, 'other');
+INSERT INTO format (format_id, format_type) VALUES (1, 'paper');
+INSERT INTO format (format_id, format_type) VALUES (2, 'digital');
+INSERT INTO format (format_id, format_type) VALUES (3, 'audiobook');
+INSERT INTO format (format_id, format_type) VALUES (4, 'read-aloud (reader)');
+INSERT INTO format (format_id, format_type) VALUES (5, 'read-aloud (listener)');
 
-INSERT INTO reading_activity (activity_id,user_id,isbn,minutes_read) VALUES (6001, 3, 9780062368683, 8);
-INSERT INTO reading_activity (activity_id,user_id,isbn,minutes_read) VALUES (6002, 2, 9781603095020, 9);
-
+INSERT INTO reading_activity (activity_id,user_id,isbn,minutes_read,format_id) VALUES (6001, 3, 9780062368683, 8, 0);
+INSERT INTO reading_activity (activity_id,user_id,isbn,minutes_read,format_id) VALUES (6002, 2, 9781603095020, 9, 0);
 
 COMMIT TRANSACTION;
